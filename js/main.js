929718db137dfbcac8a919d55ea8e49830c16bf3
Jing Yi Loh <jloh0035@student.monash.edu>
2026-09-05 14:09:12 +0800

    Added icon background

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 document.getElementById('year').textContent = new Date().getFullYear();
 
+
   // ---------- background curves (landing + hero) ----------
   function lissajous(a, b, delta, scale, cx, cy, steps){
     let d = "";
